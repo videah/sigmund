1981e2b304e70d1e5099456df1fab5fa4b9f89b6
videah <videah@selfish.systems>
2022-11-13 22:56:28 +0000

    properly set giscus theme

--- static/js/rantsona-layout.js
+++ static/js/rantsona-layout.js
@@ -11,6 +11,12 @@ var callback = function() {
         "--rantsona-offset",
         (giscus.offsetHeight + footer.offsetHeight) + "px"
     );
+
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        setCommentTheme('dark');
+    } else {
+        setCommentTheme('light');
+    }
 }
 
 const observer = new MutationObserver(callback);
